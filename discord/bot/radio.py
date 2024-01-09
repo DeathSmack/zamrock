729ba083481e51e24fbd61dd3bcb3557eb55815d
DeathSmack <105976881+DeathSmack@users.noreply.github.com>
2024-01-09 03:36:39 -0700

    Update radio.py
    Added ping command.

--- discord/bot/radio.py
+++ discord/bot/radio.py
@@ -10,36 +10,36 @@ intents.messages = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-TOKEN = 'https://deathsmack.com/'
-URL = "https://stream.zeno.fm/a10b9oqbgkevv"  # URL to play
+TOKEN = 'https://deathsmack.com'
+URL = "https://deathsmack.com"  # URL to ping
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} is online and ready.')
 
 @bot.event
-async def on_message(message):
-    if message.author.bot or not message.content.startswith(bot.command_prefix):
-        return
-    
-    await bot.process_commands(message)
+async def on_ready():
+    print(f'{bot.user.name} is online and ready.')
 
 @bot.command()
 async def ping(ctx):
-    start_time = time.time()
-    
-    async with aiohttp.ClientSession() as session:
-        async with session.get("https://deathsmack.com") as response:
-            latency = time.time() - start_time
-            if response.status == 200:
-                await ctx.send(f"Pong! Ping to DeathSmack.com: {latency*1000:.2f}ms")
-            else:
-                await ctx.send("Oops! DeathSmack.com seems to be down.")
-        
+    async with ctx.typing():
+        start_time = time.time()
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.head('https://deathsmack.com') as response:
+                    if response.status == 200:
+                        latency = (time.time() - start_time) * 1000
+                        await ctx.send(f'Pong! Your ping to ZamRock server: {latency:.2f}ms')
+                    else:
+                        await ctx.send('Oops! DeathSmack.com seems to be down.')
+        except aiohttp.ClientError:
+            await ctx.send('Oops! Something went wrong while trying to ping DeathSmack.com.')
+            
 @bot.command()
 async def play(ctx):
     global URL
-    voice_channel = bot.get_channel(1193440450904850502)  # Channel ID to connect to
+    voice_channel = bot.get_channel(1234567890)  # Channel ID to connect to
     
     if ctx.voice_client is None:
         await voice_channel.connect()
@@ -48,7 +48,7 @@ async def play(ctx):
         voice_client = bot.voice_clients[0]
         voice_client.stop()
         voice_client.play(discord.FFmpegPCMAudio(URL))
-        await ctx.send('Now playing our assortment of mixtapes from [DeathSmack.com](https://deathsmack.com/)')
+        await ctx.send(f'Now playing audio from {URL}')
     else:
         await ctx.send('Bot is currently playing a URL.')
 
@@ -56,7 +56,7 @@ async def play(ctx):
 async def stop(ctx):
     if bot.voice_clients:
         bot.voice_clients[0].stop()
-        await ctx.send('Stopped playing.')
+        await ctx.send('Stopped playing audio.')
     else:
         await ctx.send('Bot is not connected to a voice channel.')
 
@@ -64,4 +64,4 @@ async def stop(ctx):
 async def test(ctx):
     await ctx.send('Test command received and processed successfully.')
 
-bot.run('https://deathsmack.com/')
+bot.run('https://deathsmack.com')
